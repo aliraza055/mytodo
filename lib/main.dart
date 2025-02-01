a6c774185a633473d712ef9779b45df9878569d8
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-02-01 18:46:31 +0500

    change uploadimage class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mytodo/Screens/todo.dart';
+import 'package:mytodo/Screens/uploadimg.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TodoPage(),
+      home: const UploadImage(),
     );
   }
 }
