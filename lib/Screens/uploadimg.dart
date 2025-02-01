54bd719c58b9729b95c8b4821c3e3ebbcd5a364b
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-02-01 19:08:06 +0500

    add container

--- lib/Screens/uploadimg.dart
+++ lib/Screens/uploadimg.dart
@@ -16,6 +16,55 @@ class _UploadImageState extends State<UploadImage> {
         title:const Text('Upload Image'),
         centerTitle: true,
       ),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              border: Border.all()
+            ),
+            child:const Center(child: Icon(Icons.image),),
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        InkWell(
+  onTap: () {
+    // Your action
+  },
+  borderRadius: BorderRadius.circular(50),
+  child: Container(
+   // width: 60,
+    height: 60,
+    decoration: BoxDecoration(
+    //  shape: BoxShape.circle, // Makes it circular
+      gradient:const LinearGradient(
+        colors: [Colors.blue, Colors.purple], // Two colors for gradient
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          blurRadius: 10,
+          spreadRadius: 2,
+          offset:const Offset(2, 4), // Adds depth
+        ),
+      ],
+    ),
+    child:const Center(
+      child: Icon(Icons.check, color: Colors.white, size: 30),
+    ),
+  ),
+),
+
+      ],
+    ),
     );
   }
 }
