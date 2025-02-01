9cf1fc7103a00eeddf46596fa04fdd7ff54f6e2a
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-02-01 18:46:44 +0500

    remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mytodo/Screens/todo.dart';
 import 'package:mytodo/Screens/uploadimg.dart';
 
 void main() async {
